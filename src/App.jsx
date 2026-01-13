e5d3ee136abc9073eb63d91f68cd2afc65fe4a9f
zeel-90 <zeeldevani90@gmail.com>
2026-01-13 10:38:52 +0530

    and "/header" paths - Import Route and Routes components from react-router-dom - Update main.jsx formatting and import statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <div className="text-8xl">Hello world</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/header" element={<Header />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
